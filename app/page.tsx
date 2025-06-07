a71ffc626f83c8225e12ab1950fef716b5d91d83
kixixixixi <kixixixixi@gmail.com>
2025-06-07 15:31:23 +0900

    Add news

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import type { ComponentProps, FC } from "react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Card } from "../components/elements/card"
 import { Button } from "../components/elements/button"
 import { Character } from "../components/modules/character"
@@ -9,16 +9,12 @@ import { SpeechRecognitionModal } from "../components/modules/speech-recognition
 import { openai } from "../services/openai"
 import { scenarioPrompt } from "../services/prompt"
 import { useLoadingContext } from "../services/loading-provider"
+import axios from "axios"
 
 const initialTopics: Topic[] = [
   {
     title: "PC忘れ事件",
     body: "ハッカソンに出るために東京から福岡まできました。\nしかし、福岡に着いた時にPCを忘れてしまったことに気づきました。\nPCがないとハッカソンで力になれません。どうしたらいいでしょうか？",
-  },
-  {
-    title:
-      "自民党が都議選公約、「所得倍増」に向け生活支援…生理痛や更年期障害の無料検診も盛り込む",
-    body: "生活支援では物価高や家賃の上昇に備え、「東京都公式アプリ（東京アプリ）」を通じ、子育て世帯が賃貸に住む場合は月２万円分、住宅を購入した場合は１００万円分のポイントを、それぞれ付与するとした。福祉政策では女性の健康向上に焦点を当て、生理痛や更年期障害の無料検診などの実施を盛り込んだ。",
   },
 ]
 
@@ -33,6 +29,29 @@ const Page: FC<ComponentProps<"section">> = ({ ...props }) => {
   const [apiKey, setApiKey] = useState<string | undefined>(
     process.env.NEXT_PUBLIC_OPENAI_API_KEY
   )
+
+  const fetchNews = async () => {
+    const {
+      data: { articles },
+    } = await axios("https://newsapi.org/v2/everything", {
+      params: {
+        q: "米",
+        apiKey: "5dca2532a49d4b20b5689d32c025f805",
+      },
+    })
+    articles.slice(0, 5).map((item) => {
+      setTopics([
+        ...topics,
+        {
+          title: item.title,
+          body: item.description,
+        },
+      ])
+    })
+  }
+  useEffect(() => {
+    fetchNews()
+  }, [])
 
   const handleCreateScenario = async () => {
     if (!selectedTopic) return
